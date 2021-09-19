ba3212c44419f72d6b72328bdcf1152a51c69745
rtmaww <308874378@qq.com>
2021-09-20 00:38:28 +0800

    Initial commit

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python train_nyt.py --save_data_path data/ \
+CUDA_VISIBLE_DEVICES=0 python train_nyt.py --save_data_path data/nyt \
                                             --save_model_path path/to/save \
                                             --save_model_name model \
                                             --noise_label
